24877431adc419c476f706cd35d85cd97a208b04
katchy <priya.thanya@gmail.com>
2015-04-17 21:25:35 +0000

    initial_commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tasteswing::Application.config.secret_key_base = 'f7da0c62d9ee1343ed33a68527bf0f7933f3a86ceff651abfede645bb24e84da8cc7031a9bb2b6bc50c0b88e609e7d8cb7a14b2d310fab3f45ff9c68e7714d75'
