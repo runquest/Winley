6a0716ab98de8cba0f3dd6fcc7dbfe1a8ebf8d6a
runquest <aiste.ulozaite@gmail.com>
2015-04-19 16:38:34 -0700

    user routes and pathways set, edit/update methods implemented

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+ class SessionsController < ApplicationController
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user), notice: "Welcome back, #{user.fname}!"
+    else
+      flash.now[:alert] = "Log in failed..."
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to users_path, notice: "Adios!"
+  end
+
+end
