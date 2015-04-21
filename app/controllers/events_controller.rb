e2fc132302d467af0cfd84b2667eac7fcd4dd07b
katchy <priya.thanya@gmail.com>
2015-04-21 20:12:07 +0000

    event new,edit,show pages added

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,55 @@
 class EventsController < ApplicationController
   def event_home
+    @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to event_path(@event), notice: "#{@event.event_title}!"
+    else
+      render :new
+    end
+  end
+
+  def show
+     @events= Event.all
+  end
+
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to root
+  end
+
+  def edit
+      @event = Event.find(params[:id])
+  end
+
+   def update
+    @event = Event.find(params[:id])
+
+    if @event.update_attributes(user_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end 
+
+  protected
+
+  def event_params
+    
+
+
+    params.require(:event).permit(:event_title, :event_venue, :event_date, :duration, :description)
+  end
+
+
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,19 @@ class Event < ActiveRecord::Base
 	has_and_belongs_to_many :users
 	has_and_belongs_to_many :bottles
 
+    validates :event_title, 
+    presence: true
+
+    validates :event_venue,
+    presence: true
+
+    validates :event_date,
+    presence: true
+
+    validates :duration,
+    presence: true 
+    
+    validates :description,
+    presence: true
+
 end
