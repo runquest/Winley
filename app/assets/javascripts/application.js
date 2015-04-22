c729685c7d9870095b75a6ccea9e983151bf406e
Dakota Carter <dakotacar@gmail.com>
2015-04-22 22:36:03 +0000

    wrote basic API call, note the key

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,44 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var xmlhttp = new XMLHttpRequest();
+var url = "http://services.wine.com/api/beta2/service.svc/JSON/catalog?apikey=b1af7f1d65f1e1ebdb2faf060ad8fadd";
+
+xmlhttp.onreadystatechange = function() {
+  if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+    var myArr = JSON.parse(xmlhttp.responseText);
+  }
+}
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+
+console.log(xmlhttp);
+
+
+var displayBottle = function(data) {
+  // debugger
+  console.log(data);
+}
+
+
+var req = new XMLHttpRequest();
+var link = "http://services.wine.com/api/beta2/service.svc/JSON/catalog?search=Mondavi&apikey=b1af7f1d65f1e1ebdb2faf060ad8fadd"
+
+req.onreadystatechange = function() {
+  console.log('state changed', req.readyState, req.status);
+  if (req.readyState == 4 && req.status == 200) {
+
+    console.log('received success response', req.responseText);
+    var data = JSON.parse(req.responseText);
+
+    displayBottle(data);
+  }
+}
+req.open("GET", link, true);
+req.send();
+
+// var wine = req.response;
+// console.log('wine is', wine);
+
