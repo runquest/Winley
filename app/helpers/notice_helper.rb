ec780596c90a454c41ab13463dd8cba95b6f6589
runquest <aiste.ulozaite@gmail.com>
2015-04-28 02:34:26 -0700

    flash notifications

---
+++ app/helpers/notice_helper.rb
@@ -0,0 +1,19 @@
+module NoticeHelper
+
+
+
+def show_flash(options = {})
+    output = ActiveSupport::SafeBuffer.new
+
+    [:alert, :notice].each do |message|
+        output << content_tag(:p, class: [message, options[:class]], tabindex: '0') do
+            flash[message]
+        end if flash[message].present?
+
+        flash[message] = nil
+     end
+
+     output
+end
+
+end
